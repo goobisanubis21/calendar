cdeeb44da47d6c6869a626334553cebf3b7c8ae4
Anthony Parrino <anthony21parrino21@gmail.com>
2020-10-10 20:54:43 -0400

    working on getting colors to match time of day

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 // setting date variable equal to the current date and time
 var date = moment().format('dddd, MMMM Do, YYYY')
 $("#currentDay").text(date);
+
+var timeBlock = $(".time-block");
+var currentTime = moment().format('h');
+var saveButton = $(".saveBtn");
+var hour = $(".hour").text();
+
+function setColor() {
+    var currentTime = moment().format('h');
+    if (currentTime === hour) {
+        timeBlock.toggleClass("present");
+    } else if (currentTime > hour){
+        timeBlock.toggleClass("future");
+    }else {
+        timeBlock.toggleClass("past");
+    }console.log(hour)
+}
+setColor()
