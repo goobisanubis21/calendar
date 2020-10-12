00be6cd669fec9f457e183813bd5ed5e2de683c7
Anthony Parrino <anthony21parrino21@gmail.com>
2020-10-11 21:23:24 -0400

    debugging set color function

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-// setting date variable equal to the current date and time
-var date = moment().format('dddd, MMMM Do, YYYY')
-$("#currentDay").text(date);
+$(document).ready(function () {
 
-var timeBlock = $(".time-block");
-var currentTime = moment().format('h');
-var saveButton = $(".saveBtn");
-var hour = $(".hour").text();
+    // setting date variable equal to the current date and time
+    var date = moment().format('dddd, MMMM Do, YYYY')
+    $("#currentDay").text(date);
 
-function setColor() {
-    var currentTime = moment().format('h');
-    if (currentTime === hour) {
-        timeBlock.toggleClass("present");
-    } else if (currentTime > hour){
-        timeBlock.toggleClass("future");
-    }else {
-        timeBlock.toggleClass("past");
-    }console.log(hour)
-}
-setColor()
+    var timeBlock = $(".time-block");
+    var currentTime = moment().format('H');
+    var saveButton = $(".saveBtn");
+    var hour = $(".hour");
+    var allTimes = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+    // for(var i = 0; i < hour.length; i++) {
+    //     var times = hour.attr('value');
+    //     console.log(times[i])
+    // } 
+
+    function setColor() {
+        for (var i = 0; i < hour.length; i++) {
+            if (currentTime == allTimes[i]) {
+                timeBlock.toggleClass("present");
+            }
+            if (currentTime < allTimes[i]) {
+                timeBlock.toggleClass("future");
+            }
+            if (currentTime > allTimes[i]) {
+                timeBlock.toggleClass("past");
+            }
+        }console.log(allTimes)
+    }
+    setColor();
+
+    function showTask() {
+        var showTask = localStorage.getItem("task");
+        timeBlock.text(showTask);
+    }
+
+    saveButton.on("click", function () {
+        var task = $(timeBlock).val();
+        task += $(this).val();
+        localStorage.setItem("task", task);
+        console.log(task);
+        showTask()
+    })
+
+    // showTask();
+});
