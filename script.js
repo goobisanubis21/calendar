3f2b20d0c1040df0fa6ef4dd79bf12a0ff5f4ee9
Anthony Parrino <anthony21parrino21@gmail.com>
2020-10-15 12:20:24 -0400

    debugging local storage

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
+// start function when document is ready
 $(document).ready(function () {
 
     // setting date variable equal to the current date and time
     var date = moment().format('dddd, MMMM Do, YYYY')
     $("#currentDay").text(date);
 
-    var timeBlock = $(".time-block");
+    // declaring all global variables
     var currentTime = moment().hours();
     var saveButton = $(".saveBtn");
-    var hour = $(".hour");
-    var task = localStorage.getItem("task");
-    
+    var timeBlock = $(".time-block")
+
+
+    // function to set the color according the time of day
     function setColor() {
-        $(".time-block").each(function () {
+
+        // loop through each time block class and for each...
+        timeBlock.each(function () {
+
+            //set hour block variable equal to the specific id number
             var hourBlock = $(this).attr("id");
+
+            // if the current time of day is less than the id number then add the past class to the hour block textarea
             if (currentTime > hourBlock) {
-               $(this).addClass("past");
+                $(this).addClass("past");
             }
+
+            // if the current time of day is equal to the id number then remove past and add the present class to the hour block textarea
             else if (currentTime == hourBlock) {
-                console.log("futures")
                 $(this).removeClass("past");
                 $(this).addClass("present")
 
             }
+
+            // if the current time of day is less than the id number then remove present and past and add the future class to the hour block textarea
             else if (currentTime < hourBlock) {
                 $(this).removeClass("present")
                 $(this).removeClass("past");
@@ -31,22 +42,27 @@ $(document).ready(function () {
     }
     setColor();
 
-    // function showTask() {
-    //     for (var i = 0; i < hour.length; i++) {
-    //         $(".timeBlock" + i + "task").val(localStorage.getItem("task"));
-    //         timeBlock.text(task)
-    //     }
-    // }
+    showTask();
 
     saveButton.on("click", function (event) {
         event.preventDefault();
+
+        var saveButtonId = $(this).attr("id");
+        var timeBlockId = $(this).attr("id");
         var task = timeBlock.val();
-        localStorage.setItem("task", task);
-        console.log(task)
+        if (saveButtonId = timeBlockId) {
+            localStorage.setItem("task", task);
+        }
+
+
         showTask();
+        console.log(timeBlockId)
+        console.log("savebtn", saveButtonId)
     })
 
-    // showTask();
-
-    $("#9")
+    function showTask() {
+        var task = localStorage.getItem("task");
+        timeBlock.text(task);
+        
+    }
 });
